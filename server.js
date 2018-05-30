a792c6810541ddffc4abd89b2abf764417da1cac
Steven WILMOUTH <wilmouthsteven@gmail.com>
2018-05-30 13:59:32 +0200

    index.js -> server.js

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const bodyParser = require('body-parser')
 const mongo = require('./app/lib/mongo')
 
 process.env.PORT = cfg.port
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3000
 
 const mongoUrl = `mongodb://${cfg.mongo.host}:${cfg.mongo.port}/${cfg.mongo.db}`
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
