eeed64cca647f58940bc55fcabaa4e7313e1abe7
Steven WILMOUTH <wilmouthsteven@gmail.com>
2018-06-01 15:09:45 +0200

    Fix name mongodb container name (Dockerfile), insert in database badge if not own

--- app/controllers/BadgeController.js
+++ app/controllers/BadgeController.js
@@ -31,13 +31,17 @@ exports.badges = (req, res) => {
 
 exports.emit = (req, res) => {
   const badgeId = req.body.badgeId
-  // const id = req.body.recipient.id
+  const userId = req.body.recipient.id
   const email = req.body.recipient.email
   const name = req.body.recipient.name
 
   const errors = []
   if (badgeId === undefined || badgeId.length === 0) {
     errors.push('INVALID_BADGE_ID')
+  }
+
+  if (userId === undefined || userId.length === 0) {
+    errors.push('INVALID_USER_ID')
   }
 
   const regexEmail = '/^(([^<>()[].,;:@"]+(.[^<>()[].,;:@"]+)*)|(".+"))@(([^<>()[].,;:@"]+.)+[^<>()[].,;:@"]{2,})$/i'
@@ -53,25 +57,47 @@ exports.emit = (req, res) => {
     res.status(400)
     res.json({ status: 'error', data: errors })
   } else {
-    api.req({
-      method: 'POST',
-      url: `/badge/:clientId/${badgeId}`,
-      data: {
-        recipient: [ email ],
-        issued_on: moment().unix(),
-        email_subject: cfg.email.subject,
-        email_body: cfg.email.body.replace(':recipientName', name),
-        email_link_text: cfg.email.button,
-        email_footer: cfg.email.footer,
-        log_entry: {
-          client: cfg.logEntry.client,
-          issuer: cfg.logEntry.issuer
+    mongo.get('wallet').findOne({ userId }, (err, result) => {
+      if (err) return false
+
+      if (result) {
+        const found = result.badges.find((badge) => {
+          return badge.id === badgeId
+        })
+
+        if (found === undefined) {
+          const issuedOn = moment().unix()
+          api.req({
+            method: 'POST',
+            url: `/badge/:clientId/${badgeId}`,
+            data: {
+              recipient: [ email ],
+              issued_on: issuedOn,
+              email_subject: cfg.email.subject,
+              email_body: cfg.email.body.replace(':recipientName', name),
+              email_link_text: cfg.email.button,
+              email_footer: cfg.email.footer,
+              log_entry: {
+                client: cfg.logEntry.client,
+                issuer: cfg.logEntry.issuer
+              }
+            }
+          }, (error, response, body) => {
+            if (error || response.statusCode !== 201) res.json({ status: 'error', data: body.error })
+
+            mongo.get('wallet').findOneAndUpdate(
+              { userId },
+              { $push: { badges: { id: badgeId, issuedOn } } },
+              { upsert: true },
+              (err, result) => {
+                if (err) throw err
+
+                res.json({ status: 'success', data: 'BADGE_EMITTED' })
+              }
+            )
+          })
         }
       }
-    }, (error, response, body) => {
-      if (error || response.statusCode !== 201) res.json({ status: 'error', data: body.error })
-
-      res.json({ status: 'success', data: 'BADGE_EMITTED' })
     })
   }
 }
