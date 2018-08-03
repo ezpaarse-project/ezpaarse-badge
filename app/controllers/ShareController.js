61278d16b46a46905163d58216fe5e7ada130901
Steven WILMOUTH <wilmouthsteven@gmail.com>
2018-08-03 09:55:21 +0200

    CSS in html page and Image (base64) in view

--- app/controllers/ShareController.js
+++ app/controllers/ShareController.js
@@ -4,35 +4,19 @@ const moment = require('moment')
 const api = require('../lib/api')
 const mongo = require('../lib/mongo')
 
-exports.embed = async (req, res) => {
-  await getBadgeInfo(req.query.b).then(data => {
-    const badge = {
-      name: (req.query.l === 'fr' || !req.query.l) ? data.name : data.alt_language[req.query.l].name,
-      img: data.image,
-      issuedOn: 'N/A'
-    }
-
-    const link = {
-      u: req.query.u,
-      b: req.query.b,
-      l: (!req.query.l || req.query.l === 'fr') ? 'fr' : req.query.l
-    }
-
-    mongo.get('wallet').findOne({ userId: req.query.u, 'badges.id': req.query.b }, { 'badges.$': 1 }, (err, result) => {
-      if (err) return res.json({ status: 'error', data: 'NO_BADGES' })
-
-      if (result) {
-        badge.issuedOn = moment.unix(result.badges[0].issuedOn).format((!req.query.l || req.query.l === 'fr') ? 'DD/MM/YYYY' : 'YYYY-MM-DD')
-      }
-
-      const t = JSON.parse(fs.readFileSync(path.resolve(`app/locales/${(!req.query.l || req.query.l === 'fr') ? 'fr' : req.query.l}.json`)).toString())
-      res.render('embed', { badge, t, link })
-    })
-  })
+exports.embed = (req, res) => {
+  render(req, res, 'embed')
 }
 
-exports.share = async (req, res) => {
-  await getBadgeInfo(req.query.b).then(data => {
+exports.view = (req, res) => {
+  render(req, res, 'view')
+}
+
+function render (req, res, view) {
+  api.req({ method: 'GET', url: `/badge/_/${req.query.b}.json?v=2.0` }, (error, response, body) => {
+    if (error) return res.json({ status: 'error', data: 'BADGE_NOT_FOUND' })
+
+    const data = JSON.parse(body)
     const badge = {
       id: req.query.b,
       name: (req.query.l === 'fr' || !req.query.l) ? data.name : data.alt_language[req.query.l].name,
@@ -49,6 +33,14 @@ exports.share = async (req, res) => {
       url: `${req.protocol}://${req.headers.host}`
     }
 
+    const style = {
+      css: fs.readFileSync(path.resolve(`public/css/${view}.css`), 'utf-8'),
+      img: {
+        ang: fs.readFileSync(path.resolve('public/img/ang.png'), 'base64'),
+        obf: fs.readFileSync(path.resolve('public/img/obf.png'), 'base64')
+      }
+    }
+
     mongo.get('wallet').findOne({ userId: req.query.u, 'badges.id': req.query.b }, { 'badges.$': 1 }, (err, result) => {
       if (err) return res.json({ status: 'error', data: 'NO_BADGES' })
 
@@ -56,18 +48,8 @@ exports.share = async (req, res) => {
         badge.issuedOn = moment.unix(result.badges[0].issuedOn).format((!req.query.l || req.query.l === 'fr') ? 'DD/MM/YYYY' : 'YYYY-MM-DD')
       }
 
-      const t = JSON.parse(fs.readFileSync(path.resolve(`app/locales/${(!req.query.l || req.query.l === 'fr') ? 'fr' : req.query.l}.json`)).toString())
-      res.render('view', { badge, t, link })
-    })
-  })
-}
-
-function getBadgeInfo (badgeId) {
-  return new Promise((resolve, reject) => {
-    api.req({ method: 'GET', url: `/badge/_/${badgeId}.json?v=2.0` }, (error, response, body) => {
-      if (error) return reject(error)
-
-      resolve((body.length > 0) ? JSON.parse(body) : null)
+      const t = JSON.parse(fs.readFileSync(path.resolve(`app/locales/${(!req.query.l || req.query.l === 'fr') ? 'fr' : req.query.l}.json`), 'utf-8'))
+      res.render(`${view}`, { badge, t, link, style })
     })
   })
 }
