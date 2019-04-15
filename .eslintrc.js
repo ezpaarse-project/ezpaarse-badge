caa2b92adf7759360188e98f8d567c3db0338c7a
wilmouths <wilmouthsteven@gmail.com>
2019-04-15 14:46:06 +0200

    Usage of airbnb standards

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: 'standard',
-  // add your custom rules here
-  rules: {
-    'no-warning-comments': 1,
-    'prefer-const': 2,
-    'no-var': 2,
-    'prefer-arrow-callback': 2,
-    'prefer-template': 1,
-    'arrow-parens': 0, // allow paren-less arrow functions
-    'no-console': 2, // do not allow console.logs
-    'no-multi-spaces': [2, {
-    exceptions: {
-        VariableDeclarator: true,
-        ImportDeclaration: true,
-        Property: true
-      }
-    }]
-  },
-  globals: {}
-}
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "no-await-in-loop": 0
+    }
+};
